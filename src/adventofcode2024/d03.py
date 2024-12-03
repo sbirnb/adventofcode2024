0729e0525f35d8bfb5260775e6bf09ff917a8325
Sam Birnbaum <birnbaum.sam@gmail.com>
2024-12-03 18:04:00 +0100

    Day 3, part2 single regex

--- src/adventofcode2024/d03.py
+++ src/adventofcode2024/d03.py
@@ -6,16 +6,12 @@ def parse_input(input_: Iterable[str]) -> str:
     return ''.join(input_)
 
 
-def sum_muls(s: str) -> int:
-    return sum(int(v1) * int(v2) for v1, v2 in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', s))
-
-
 def part1(input_: Iterable[str]) -> int:
-    return sum_muls(parse_input(input_))
+    return sum(int(v1) * int(v2) for v1, v2 in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', parse_input(input_)))
 
 
 def part2(input_: Iterable[str]) -> int:
-    return sum(sum_muls(do.split('don\'t()', 1)[0]) for do in parse_input(input_).split('do()'))
+    return sum(int(v1) * int(v2) for v1, v2 in re.findall(r'don\'t\(\)(?:(?!do\(\)).)*|mul\((\d{1,3}),(\d{1,3})\)', parse_input(input_), re.DOTALL) if v1)
 
 
 if __name__ == '__main__':
@@ -23,4 +19,3 @@ if __name__ == '__main__':
         input_ = tuple(fi)
     print(part1(input_))
     print(part2(input_))
-
