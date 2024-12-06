98f1b65e4aff9b761de407ef45f35ebf9606fd42
Sam Birnbaum <sam.birnbaum@booking.com>
2024-12-06 12:52:45 +0100

    Cleanup day06

--- src/adventofcode2024/d06.py
+++ src/adventofcode2024/d06.py
@@ -1,43 +1,43 @@
-from typing import Iterable, Tuple, Mapping, Iterator
+from typing import Iterable, Tuple, Iterator, Sequence, FrozenSet
 
 
-def parse_input(input_: Iterable[str]) -> Mapping[complex, str]:
-    return {row + col * 1j: cell for row, cells in enumerate(input_) for col, cell in enumerate(cells.strip())}
+def parse_input(input_: Sequence[str]) -> Tuple[int, int, complex, FrozenSet[complex]]:
+    return len(input_), len(input_[0]), \
+        next(row + col * 1j for row, cells in enumerate(input_) for col, cell in enumerate(cells) if cell == '^'), \
+        frozenset(row + col * 1j for row, cells in enumerate(input_) for col, cell in enumerate(cells) if cell == '#')
 
 
-def get_guard_states(map_: Mapping[complex, str], state0: Tuple[complex, complex]) -> Iterator[Tuple[complex, complex]]:
+def get_guard_states(height: int, width: int, obstacles: FrozenSet[complex], state0: Tuple[complex, complex]) -> Iterator[Tuple[complex, complex]]:
     pos, dir_ = state0
-    while pos in map_:
+    while 0 <= pos.real < height and 0 <= pos.imag < width:
         yield pos, dir_
-        while map_.get(pos + dir_) == '#':
+        while pos + dir_ in obstacles:
             dir_ *= -1j
         pos += dir_
 
 
 def part1(input_: Iterable[str]) -> int:
-    map_ = parse_input(input_)
-    pos0 = next(iter(coord for coord, cell in map_.items() if cell == '^'))
-    return len(set(pos for pos, _ in get_guard_states(map_, (pos0, -1))))
+    height, width, pos0, obstacles = parse_input(tuple(input_))
+    return len(set(pos for pos, _ in get_guard_states(height, width, obstacles, (pos0, -1))))
 
 
 def part2(input_: Iterable[str]) -> int:
-    map_ = parse_input(input_)
-    pos0 = next(iter(coord for coord, cell in map_.items() if cell == '^'))
-    path = tuple(get_guard_states(map_, (pos0, -1)))
+    height, width, pos0, obstacles = parse_input(tuple(input_))
+    path = tuple(get_guard_states(height, width, obstacles, (pos0, -1)))
     visited_states = set()
     visited_pos = {pos0}
     obstacle_count = 0
 
-    def has_cycle(test_map: Mapping[complex, str], test_state0: Tuple[complex, complex]) -> bool:
+    def has_cycle(test_obstacles: FrozenSet[complex], state0: Tuple[complex, complex]) -> bool:
         test_visited_states = set()
-        for state in get_guard_states(test_map, test_state0):
+        for state in get_guard_states(height, width, test_obstacles, state0):
             if state in visited_states or state in test_visited_states:
                 return True
             test_visited_states.add(state)
         return False
 
     for (pos, dir_), last_state in zip(path[1:], path):
-        obstacle_count += pos not in visited_pos and has_cycle({**map_, pos: '#'}, last_state)
+        obstacle_count += pos not in visited_pos and has_cycle(obstacles | {pos}, last_state)
         visited_states.add(last_state)
         visited_pos.add(last_state[0])
 
