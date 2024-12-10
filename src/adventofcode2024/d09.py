e73e2cb7d17d6f53c903ec9fc4d7a0c7e514f9c6
Sam Birnbaum <birnbaum.sam@gmail.com>
2024-12-10 01:22:19 +0100

    D09: refactor p2, improve p1 performance

--- src/adventofcode2024/d09.py
+++ src/adventofcode2024/d09.py
@@ -1,4 +1,4 @@
-import cProfile
+from collections import deque
 from heapq import heappush, heappop
 from itertools import chain, accumulate
 from typing import Iterable, Sequence, Tuple, Iterator
@@ -8,50 +8,67 @@ def parse_input(input_: Iterable[str]) -> Sequence[int]:
     return tuple(map(int, next(iter(input_))))
 
 
+def sum_range(start: int, width: int) -> int:
+    return ((start + width - 1) * (start + width) - ((start - 1) * start)) // 2
+
+
 def part1(input_: Iterable[str]) -> int:
-    expand = tuple(chain.from_iterable([i // 2 if i % 2 == 0 else None] * v for i, v in enumerate(parse_input(input_))))
-    left = 0
-    right = len(expand) - 1
-    total = 0
-    compacted = []
-    while left <= right:
-        if expand[left] is not None:
-            total += left * expand[left]
-            compacted.append(expand[left])
-            left += 1
-        elif expand[right] is not None:
-            total += left * expand[right]
-            compacted.append(expand[right])
-            right -= 1
-            left += 1
+    widths = parse_input(input_)
+    free_space = deque()
+    fragmented_blocks = []
+    for i, (width, fs_ptr) in enumerate(zip(widths, accumulate(chain((0,), widths)))):
+        if i % 2:
+            free_space.append((fs_ptr, width))
         else:
-            right -= 1
-    return total
+            fragmented_blocks.append((i // 2, fs_ptr, width))
+    free_space.append((float('inf'), float('inf')))
+
+    def defragment() -> Iterator[Tuple[int, int, int]]:
+        iter_blocks = reversed(fragmented_blocks)
+        for index, start, width in iter_blocks:
+            if width == 0:
+                continue
+            while width > 0:
+                if start > free_space[0][0]:
+                    free_start, free_width = free_space.popleft()
+                    segment_width = min(width, free_width)
+                    yield index, free_start, segment_width
+                    width -= segment_width
+                else:
+                    yield index, start, width
+                    yield from iter_blocks
+                    return
+            free_start += segment_width
+            free_width -= segment_width
+            if free_width > 0:
+                free_space.appendleft((free_start, free_width))
+
+    return sum(index * sum_range(start, width) for index, start, width in defragment())
 
 
 def part2(input_: Iterable[str]) -> int:
     widths = parse_input(input_)
-    free_heaps = [[] for _ in range(10)]
+    free_heaps = [([float('inf')], i) for i in range(10)]
     fragmented_blocks = []
 
     for i, (width, fs_ptr) in enumerate(zip(widths, accumulate(chain((0,), widths)))):
         if i % 2:
-            heappush(free_heaps[width], fs_ptr)
+            heappush(free_heaps[width][0], fs_ptr)
         else:
             fragmented_blocks.append((i // 2, fs_ptr, width))
 
     def defragment() -> Iterator[Tuple[int, int, int]]:
         for index, start, width in reversed(fragmented_blocks):
-            try:
-                free_width, free_heap = min(((w, heap) for w, heap in enumerate(free_heaps[width:], width) if heap and heap[0] < start), key=lambda wh: wh[1][0])
-            except ValueError:
+            free_heap, free_width = min(free_heaps[width:])
+            if free_heap[0] > start:
                 yield index, start, width
             else:
                 free_start = heappop(free_heap)
                 if width < free_width:
-                    heappush(free_heaps[free_width - width], free_start + width)
+                    heappush(free_heaps[free_width - width][0], free_start + width)
                 yield index, free_start, width
-    return sum(index * sum(range(start, start + width)) for index, start, width in defragment())
+
+    return sum(index * sum_range(start, width) for index, start, width in defragment())
 
 
 if __name__ == '__main__':
